35567819332d66a86a8f2a9399ee624997ed43e1
xiaoyeyexiao <2939427731@qq.com>
2024-06-04 15:56:46 +0800

    first commit

---
+++ semilearn/__init__.py
@@ -0,0 +1,3 @@
+from .core.utils import get_dataset, get_data_loader, get_net_builder
+from .algorithms import get_algorithm
+from .datasets.cv_datasets.datasetbase import BasicDataset
---
+++ semilearn/datasets/__init__.py
@@ -0,0 +1,4 @@
+from semilearn.datasets.cv_datasets import get_cifar_openset, get_imagenet30, \
+    svhn_as_ood, lsun_as_ood, gaussian_as_ood, uniform_as_ood
+
+from semilearn.datasets.samplers import DistributedSampler, ImageNetDistributedSampler
